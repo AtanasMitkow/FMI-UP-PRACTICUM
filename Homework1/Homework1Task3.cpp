deff8c6681cea344ba1ac558e8a31950004905cc
AtanasMitkow <amatanaso1@uni-sofia.bg>
2016-10-29 00:04:36 +0300

    Homework1-Task3

---
+++ Homework1/Homework1Task3.cpp
@@ -0,0 +1,17 @@
+// Homework1Task3.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int num1, num2, sum , finalAnswer;
+	cout << "Insert 2 numbers: ";
+	cin >> num1 >> num2;
+	sum = num1 + num2;
+	cout << "The end result is: " << finalAnswer << endl;
+    return 0;
+}
+
