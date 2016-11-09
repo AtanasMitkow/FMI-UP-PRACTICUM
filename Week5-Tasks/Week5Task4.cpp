9bf9cd50227e98fdb706e351d01c5ea6bc4acae1
Atanas Mitkov <amatanaso1@uni-sofia.bg>
2016-11-09 15:33:31 +0200

    Week5-Task4

---
+++ Week5-Tasks/Week5Task4.cpp
@@ -0,0 +1,25 @@
+// Week5Task4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char sym;
+	do
+	{
+		cout << "Insert a symbol: ";
+		cin >> sym;
+	} while (sym != '!');
+	if (sym = '!')
+	{
+		cout << "Congratulations smart boy!" << endl;
+	}
+	
+	
+	return 0;
+}
+
